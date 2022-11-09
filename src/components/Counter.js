780b75a7d28008ca46dcd7f947342cbcdba750f6
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2022-11-09 17:33:50 +0400

    counter design

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,22 +1,26 @@
 import { useState } from "react";
+import "./Counter.css";
 
 const Counter = () => {
   let [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Count : {count}</h1>
-      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
-      <button disabled={!count} onClick={() => setCount(0)}>
-        Reset
-      </button>
-      <button
-        disabled={!count}
-        onClick={() => setCount((prev) => (prev <= 0 ? 0 : prev - 1))}
-      >
-        -
-      </button>
-    </>
+    <div className="container">
+      <h1>Counter</h1>
+      <h2>{count}</h2>
+      <div className="buttons">
+        <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+        <button className="reset" disabled={!count} onClick={() => setCount(0)}>
+          Reset
+        </button>
+        <button
+          disabled={!count}
+          onClick={() => setCount((prev) => (prev <= 0 ? 0 : prev - 1))}
+        >
+          -
+        </button>
+      </div>
+    </div>
   );
 };
 
